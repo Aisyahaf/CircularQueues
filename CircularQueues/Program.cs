6a9546114a9c07fb83166842738cbc7aff61940c
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-12-14 16:58:02 +0700

    Add Variable

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace CircularQueues
 {
-    internal class Program
+    class Queues
     {
-        static void Main(string[] args)
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        public Queues()
         {
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        public void insert(int element)
+        {
+            if((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+
+            queue_array[REAR] = element;
         }
     }
 }
